a3cebdc35182cd1f8779e807d22c2457e3b61dc6
tangciwei <tangciwei@baidu.com>
2017-03-31 16:50:41 +0800

    初步完成fis3组合功能

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node --harmony
+var exec = require('child_process').exec;
+exec('echo 1&&cd ../ && mkdir test',function (err, stdout) {
+	if(err){
+		return console.error(err)
+	}
+	console.log(stdout)
+})
+
 
 
